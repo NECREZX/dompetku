c7ed79609b0f27fc6fc0729cd96ce7dd90af05f7
rifqithoohaa <162125340+NECREZX@users.noreply.github.com>
2026-04-26 14:04:50 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dompetku-v1';
+const CACHE_NAME = 'dompetku-v2'; // Naikkan versi ke v2
 const assets = [
   './',
   './index.html',
@@ -10,15 +10,35 @@ const assets = [
   './js/app.js',
   './js/storage.js',
   './js/ui.js',
+  './js/format.js',
+  './js/theme.js',
+  './js/export.js',
+  './js/keuangan/sumber.js',
+  './js/keuangan/transaksi.js',
+  './js/keuangan/riwayat.js',
+  './js/tabungan/tujuan.js',
+  './js/tabungan/riwayat.js',
+  './js/tabungan/kelola.js',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Paksa service worker baru aktif
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
