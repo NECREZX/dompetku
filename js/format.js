ba9d9dcbc361e38a89a025acc02fefec341a6404
rifqithoohaa <162125340+NECREZX@users.noreply.github.com>
2026-04-27 16:06:17 +0700

    Update format.js

--- js/format.js
+++ js/format.js
@@ -12,9 +12,20 @@ const Format = {
         return new Date(dateStr).toLocaleDateString('id-ID', options);
     },
 
-    toHTMLDate(date) {
-        return date.toISOString().split('T')[0];
+    dateTime(dateStr) {
+        const options = { 
+            year: 'numeric', month: 'short', day: 'numeric',
+            hour: '2-digit', minute: '2-digit'
+        };
+        return new Date(dateStr).toLocaleDateString('id-ID', options);
     },
+
+    toHTMLDate(date) {
+        const offset = date.getTimezoneOffset() * 60000;
+        const localDate = new Date(date.getTime() - offset);
+        return localDate.toISOString().split('T')[0];
+    },
+
 
     dateMonth(dateStr) {
         const options = { year: 'numeric', month: 'long' };
